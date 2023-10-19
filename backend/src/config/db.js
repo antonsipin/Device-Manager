0b1ca4bd8e9cb1cfad268992c3cc5c516e3b5fc0
Anton Sipin <anton.sipin@gmail.com>
2023-10-19 16:38:10 +0300

    minor

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,6 @@
-const Sequilize = require('sequelize')
+const Sequelize = require('sequelize')
 
-const sequelize = new Sequilize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.HOST,
   dialect: 'postgres'
 })
