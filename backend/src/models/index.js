e68217f068b397346f3b0991a91e3217d36d2f3c
Anton <anton.sipin@gmail.com>
2023-11-07 17:19:00 +0300

    minor

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -24,12 +24,12 @@ fs
       file !== basename &&
       file.slice(-3) === '.js' &&
       file.indexOf('.test.js') === -1
-    );
+    )
   })
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
     db[model.name] = model
-  });
+  })
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
