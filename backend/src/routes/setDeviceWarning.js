cdad5c2d61a3c3bb6310ab092a85a81957bafda7
Anton <anton.sipin@gmail.com>
2023-11-27 15:12:50 +0300

    Refactoring

--- backend/src/routes/setDeviceWarning.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const deviceController = require('../controllers/device-controller')
-
-router
-  .route('/')
-  .post(deviceController.setDeviceWarning)
-
-module.exports = router
